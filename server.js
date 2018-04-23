3a201f8d69fd1cad99477dbb11de0ae12fb7ca1c
Molly Wojcik <mollywojcik@gmail.com>
2018-04-22 19:11:52 -0600

    updated connection settings for heroku deploy

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+var databaseUri = 'mongodb://localhost/reactarticlelist';
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI_;)
+} else {
+  mongoose.connect(databaseUri);
 }
 
 // Send every request to the React app
