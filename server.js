f8bd6e19839b0021a8a509e06d9de20910b74a02
Molly Wojcik <mollywojcik@gmail.com>
2018-04-23 18:17:30 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,30 +1,54 @@
+// const express = require("express");
+// const path = require("path");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const routes = require("./routes");
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+//
+// // var databaseUri = 'mongodb://localhost/reactarticlelist';
+// //
+// // if (process.env.MONGODB_URI) {
+// //   mongoose.connect(process.env.MONGODB_URI_);
+// // } else {
+// //   mongoose.connect(databaseUri);
+// // }
+//
+// // Send every request to the React app
+// // Define any API routes before this runs
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+//
+// app.listen(PORT, function() {
+//   console.log(`🌎 ==> Server now on port ${PORT}!`);
+// });
+
+
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets
+app.use(express.static("client/build"));
+// Add routes, both API and view
+app.use(routes);
 
-var databaseUri = 'mongodb://localhost/reactarticlelist';
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
-if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI_);
-} else {
-  mongoose.connect(databaseUri);
-}
-
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
+// Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
